202c3282ba2afe171d8e231d890409b4e14c5af4
James Pirruccello <jamesp@broadinstitute.org>
2018-08-27 12:27:16 -0400

    There is no need for a DEFAULT_MIN_CPU_PLATFORM and it's actually harmful to try to set it, since it varies across zones and can simply be left empty if no specific platform is needed.

--- dsub/providers/google_v2.py
+++ dsub/providers/google_v2.py
@@ -549,8 +549,6 @@ class GoogleV2JobProvider(base.JobProvider):
     ]
     network = google_v2_pipelines.build_network(None, None)
     machine_type = job_resources.machine_type or job_model.DEFAULT_MACHINE_TYPE
-    # TODO: Determine whether you must specify a default or if None is OK here
-    # min_cpu_platform = job_resources.min_cpu_platform or job_model.DEFAULT_MIN_CPU_PLATFORM
     min_cpu_platform = job_resources.min_cpu_platform
     accelerators = None
     if job_resources.accelerator_type:
